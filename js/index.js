74c9326b2c1fe26605c0bc4af80eb4e0f04dce44
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2025-03-26 15:48:35 +0200

    fix faq

--- js/index.js
+++ js/index.js
@@ -3,21 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".faq__head__content").forEach((item) => {
     item.addEventListener("click", () => {
       let parent = item.parentElement;
-
-      // Закрываем другие
-      document.querySelectorAll(".faq__list__item").forEach((faq) => {
-        if (faq !== parent) {
-          faq.classList.remove("active");
-          faq.querySelector("img").src = "./img/faq__open.png";
-        }
-      });
-
-      // Переключаем активность
       let isActive = parent.classList.toggle("active");
 
-      // Меняем картинку
       let img = item.querySelector("img");
-      img.src = isActive ? "./img/faq__close.png" : "./img/faq__open.png";
+
+      img.style.opacity = "0"; 
+      setTimeout(() => {
+        img.src = isActive ? "./img/faq__close.png" : "./img/faq__open.png";
+        img.style.opacity = "1"; 
+      }, 300);
     });
   });
 });
+
